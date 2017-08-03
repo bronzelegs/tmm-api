9708302f07c5244bcd1c08e2affcac4308d61e8f
bronzelegs <tdavis0525@gmail.com>
2017-08-03 16:29:05 -0700

    added password field

--- models/index-compiled.js
+++ models/index-compiled.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+	value: true
 });
 exports.Profile = exports.History = undefined;
 
@@ -12,23 +12,24 @@ var _mongoose2 = _interopRequireDefault(_mongoose);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var HistorySchema = new _mongoose2.default.Schema({
-  userId: { type: _mongoose2.default.Schema.Types.ObjectId, ref: '_id' },
-  action: String,
-  timeStamp: { type: Date, default: Date.now }
+	userId: { type: _mongoose2.default.Schema.Types.ObjectId, ref: '_id' },
+	action: String,
+	timeStamp: { type: Date, default: Date.now }
 });
 
 var ProfileSchema = new _mongoose2.default.Schema({
-  userName: String,
-  firstName: String,
-  lastName: String,
-  middleName: String,
-  email1: String,
-  email2: String,
-  image: String,
-  url: String,
-  about: String,
-  createDate: { type: Date, default: Date.now },
-  lastAccess: { type: Date, default: Date.now }
+	userName: String,
+	firstName: String,
+	lastName: String,
+	middleName: String,
+	email1: String,
+	email2: String,
+	image: String,
+	url: String,
+	about: String,
+	password: String,
+	createDate: { type: Date, default: Date.now },
+	lastAccess: { type: Date, default: Date.now }
 });
 
 var History = _mongoose2.default.model('History', HistorySchema);
--- models/index.js
+++ models/index.js
@@ -1,28 +1,28 @@
 import mongoose from 'mongoose';
 
 
-
 const HistorySchema = new mongoose.Schema({
-      userId: {type: mongoose.Schema.Types.ObjectId, ref: '_id'},
-      action: String,
-      timeStamp: {type: Date, default: Date.now}
+	userId: {type: mongoose.Schema.Types.ObjectId, ref: '_id'},
+	action: String,
+	timeStamp: {type: Date, default: Date.now}
 });
 
 const ProfileSchema = new mongoose.Schema({
-  userName: String,
-  firstName: String,
-  lastName: String,
-  middleName: String,
-  email1: String,
-  email2: String,
-  image: String,
-  url: String,
-  about: String,
-  createDate: {type: Date, default: Date.now},
-  lastAccess: {type: Date, default: Date.now}
+	userName: String,
+	firstName: String,
+	lastName: String,
+	middleName: String,
+	email1: String,
+	email2: String,
+	image: String,
+	url: String,
+	about: String,
+	password: String,
+	createDate: {type: Date, default: Date.now},
+	lastAccess: {type: Date, default: Date.now}
 });
 
 const History = mongoose.model('History', HistorySchema);
 const Profile = mongoose.model('Profile', ProfileSchema);
 
-export { History, Profile}
+export {History, Profile}
